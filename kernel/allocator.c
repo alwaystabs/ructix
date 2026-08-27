564efbb886cbf3afa240b24f4b8a39abadfdabea
alwaystabs <reservedstep@gmail.com>
2026-08-27 19:19:45 +0300

    allocator: add DEBUG logging and heap alignment
    - Wrap kmalloc debug output in #ifdef DEBUG
    - Rename buf to alloc_buf (separate from kernel_buf)
    - Align heap_ptr in kmalloc_init() for proper 8-byte alignment

--- kernel/allocator.c
+++ kernel/allocator.c
@@ -1,32 +1,36 @@
 #include "include/memory.h"
-#include <stdint.h>
 #include "include/panic.h"
+#include <stdint.h>
 
 extern char _heap_start[];
 extern char _heap_end[];
 static char *heap_ptr;
-extern char buf[32];
+char alloc_buf[32];
 void kmalloc_init(void) {
-    heap_ptr = _heap_start;
-
+  heap_ptr = (char *)(((uintptr_t)_heap_start + 7) & ~7);
+  ;
 }
 void *kmalloc(size_t size) {
-    size_t aligned_size = (size + 7) & ~7; // Formula allocates 8 bytes anyways (but 0 gives 0), which is required by RISC-V
-    if (heap_ptr + aligned_size > _heap_end) { // Overflow
-        panic("Out of Memory\n");
-    }
-    void *ptr = heap_ptr;
-    heap_ptr += aligned_size;
-    print("Requested ");
-    itoa(size, buf);
-    print(buf);
-    print(" bytes, allocated ");
-    itoa(aligned_size, buf);
-    print(buf);
-    print(" bytes");
-    return ptr;
+  size_t aligned_size =
+      (size + 7) & ~7; // Formula allocates 8 bytes anyways (but 0 gives 0),
+                       // which is required by RISC-V
+  if (heap_ptr + aligned_size > _heap_end) { // Overflow
+    panic("Out of Memory\n");
+  }
+  void *ptr = heap_ptr;
+  heap_ptr += aligned_size;
+  #ifdef DEBUG
+  print("Requested ");
+  itoa(size, alloc_buf);
+  print(alloc_buf);
+  print(" bytes, allocated ");
+  itoa(aligned_size, alloc_buf);
+  print(alloc_buf);
+  print(" bytes");
+  #endif
+  return ptr;
 }
 void kfree(void *ptr) {
-    (void)ptr;
-    return;
+  (void)ptr;
+  return;
 }
