14be0c97e650d9e28f1c09d66af651b56ae3f66f
alwaystabs <reservedstep@gmail.com>
2026-09-01 21:29:13 +0300

    allocator: free-list allocator with freeing memory option

--- kernel/include/memory.h
+++ kernel/include/memory.h
@@ -6,8 +6,15 @@
 #include "uart.h"
 #include <stddef.h>
 
+typedef struct block_header {
+    size_t size;
+    struct block_header *next;
+    int used;
+} block_header_t;
+
 void kmalloc_init(void);
 void *kmalloc(size_t size);
 void kfree(void *ptr);
+void test_allocator(void);
 
 #endif
