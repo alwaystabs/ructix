dd687fc79205fa1a670241da86d121b9bdc127f7
alwaystabs <reservedstep@gmail.com>
2026-09-09 22:21:45 +0300

    trap handler: added another cases to catch

--- kernel/unhandled_trap.c
+++ kernel/unhandled_trap.c
@@ -22,25 +22,22 @@ void unhandled_trap_c(uint64_t cause, uint64_t epc) {
 
     print("\033[5;1H║  CAUSE: ");
     switch (cause) {
-    case 0:
-        print("Instruction Address Misaligned");
-        break;
-    case 2:
-        print("Illegal Instruction");
-        break;
-    case 4:
-        print("Load Address Misaligned");
-        break;
-    case 6:
-        print("Store Address Misaligned");
-        break;
-    case 13:
-        print("Load Page Fault");
-        break;
-    default:
-        print("Unknown Exception");
-        break;
-    }
+    case 0:  print("Instruction Address Misaligned"); break;
+    case 1:  print("Instruction Access Fault");       break;
+    case 2:  print("Illegal Instruction");            break;
+    case 3:  print("Breakpoint");                     break;
+    case 4:  print("Load Address Misaligned");        break;
+    case 5:  print("Load Access Fault");              break;
+    case 6:  print("Store Address Misaligned");       break;
+    case 7:  print("Store Access Fault");             break;
+    case 8:  print("User Environment Call");          break;
+    case 9:  print("Supervisor Environment Call");    break;
+    case 11: print("Machine Environment Call");       break;
+    case 12: print("Instruction Page Fault");         break;
+    case 13: print("Load Page Fault");                break;
+    case 15: print("Store Page Fault");               break;
+    default: print("Unknown Exception");              break;
+}
     print("\033[5;68H║");
 
     print("\033[6;1H║  mcause: ");
