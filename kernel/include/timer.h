dd582e7f42c4442ac417c1d90da2655c56ec6e34
alwaystabs <reservedstep@gmail.com>
2026-09-20 00:23:58 +0300

    fix(timer): reorder defines, add stdint include
    - TIMER_FREQ before TIMER_INTERVAL
    - include <stdint.h> for uint64_t
    - MTIME derived from MTIME_ADDR

--- kernel/include/timer.h
+++ kernel/include/timer.h
@@ -1,12 +1,15 @@
 #ifndef TIMER_H
 #define TIMER_H
 
-#define MTIME_ADDR  0x0200BFF8
-#define MTIMECMP_ADDR 0x02004000
-#define TIMER_INTERVAL (TIMER_FREQ / TICKS_PER_SEC)
-#define MTIME ((volatile uint64_t *)0x0200BFF8)
-#define MTIMECMP ((volatile uint64_t *)0x02004000)
+#include <stdint.h>
+
 #define TIMER_FREQ 10000000
-#define TICKS_PER_SEC 60
+#define TICKS_PER_SEC 100
+#define TIMER_INTERVAL (TIMER_FREQ / TICKS_PER_SEC)
+
+#define MTIMECMP_ADDR 0x02004000
+#define MTIME_ADDR  0x0200BFF8
+#define MTIME ((volatile uint64_t *)MTIME_ADDR)
+#define MTIMECMP ((volatile uint64_t *)0x02004000)
 
 #endif
